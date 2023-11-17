411cdd771e551b5f497857414eac52b81d194fab
IntellectualSpecialist <intellectualspecialist@gmail.com>
2023-11-18 02:10:05 +0500

    Добавляет скрипт

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let menuButton = document.querySelector('.menu-burger-button');
+let popover = document.querySelector('.popover');
+let popoverCloseButton = document.querySelector('.popover-close-button');
+
+menuButton.onclick = function (evt) {
+  evt.preventDefault();
+
+  popover.classList.add('popover-shown');
+}
+
+popoverCloseButton.onclick = function () {
+  popover.classList.remove('popover-shown');
+}
